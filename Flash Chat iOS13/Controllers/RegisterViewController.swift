8c122d09c7d8c46123ab865dbf908cf8a2f48d43
MityaAndreevich <dmitry.logachev@icloud.com>
2022-01-22 21:04:50 +0300

    registration process added pop up for error added

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,28 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    self.sendingAlertMessage(title: "Smth wrong!", message: "\(e.localizedDescription)")
+                } else {
+                    //Navigate to the ChatVC
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
+    //MARK: - Private Methods
+    private func sendingAlertMessage(title: String, message: String) {
+        let alertMessage = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertMessage.addAction(okAction)
+        present(alertMessage, animated: true)
+    }
 }
