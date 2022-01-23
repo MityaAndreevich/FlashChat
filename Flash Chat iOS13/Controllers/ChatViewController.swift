f97397b06fc4be9010f2a15a0b8f680a1ac4846d
MityaAndreevich <dmitry.logachev@icloud.com>
2022-01-23 17:28:46 +0300

    code reduction was made

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -16,7 +16,7 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "ChatIsOn"
+        title = K.appName
         navigationItem.hidesBackButton = true
     }
     
