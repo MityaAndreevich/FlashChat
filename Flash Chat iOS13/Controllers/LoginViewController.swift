8cc09d905ccdd7be09ef1534e3380b715213d601
MityaAndreevich <dmitry.logachev@icloud.com>
2022-01-23 14:10:26 +0300

    login process added

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +16,30 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    self.sendingAlertMessage(title: "Oops...", message: "\(e.localizedDescription)")
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
-    
+    //MARK: - Private Methods
+    private func sendingAlertMessage(title: String, message: String) {
+        let alertMessage = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "Try again", style: .default)
+        alertMessage.addAction(okAction)
+        let registerAction = UIAlertAction(title: "Register", style: .default) { action in
+            self.performSegue(withIdentifier: "BackToRegister", sender: self)
+        }
+        alertMessage.addAction(registerAction)
+        present(alertMessage, animated: true)
+    }
 }
