5eaaddd975c2f6a7d6231365c9f063f0e3e91e24
MityaAndreevich <dmitry.logachev@icloud.com>
2022-01-30 20:11:11 +0300

    automatic scrolling to the last message added

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -101,6 +101,8 @@ extension ChatViewController {
                             self.messages.append(newMessage)
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
                         }
                     }
